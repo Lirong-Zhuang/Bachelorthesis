25edcb72b848ef37e62ac4e572b0865b8a1febb2
Lirong-Zhuang <zhuang.lirong@outlook.com>
2024-10-20 14:25:50 +0200

    Update oilcooling_filling.cpp
    The motor rotor is stationary, 12 windings (parameters) and 5 oil inlets (fixed) are complete, gravity, density and global dimensions are to be agreed upon

--- oilcooling_filling.cpp
+++ oilcooling_filling.cpp
@@ -17,25 +17,30 @@ Real WL = 30 / 10;  /**< Winding length. */
 Real WH = 25 / 10;  /**< Winding height. */
 Real AG = 1 / 10;   /**< Air-gap. */
 Real LL = 2.8 / 10; /**< Inflow region length. */
+Real DO = 10 / 10;  /**< Outflow diameter. */
 
 Real PI = 3.1415926;
 Real RM = 0.5 * DM;                      /**< Radius of motor hausing. */
 Real RR = 0.5 * DR;                      /**< Radius of rotor. */
 Real RS = 0.5 * DS;                      /**< Radius of transmission shaft on rotor. */
 Real Wnum = 12;                          /**< Winding Number. */
+Real angle_increment = 2 * PI / Wnum;
 Real WD = RR + AG + 0.5 * WH;            /**< Distance from center point to the center of a winding. */
 Real ZW = RR + AG;                       /**< Distance from center point to the site of a winding. */
 int resolution_circle = 200;             /**<Approximate the circle as the number of sides of the polygon. */
 Real resolution_ref = 0.025;             /**< Initial reference particle spacing. */
 Real BW = resolution_ref * 4;            /**< Extending width for wall boundary. */
 Real LH = 2.0 * BW;                      /**< Inflows region height. */
+Real OH = LH;                            /**< Outflows region height. */
 Real Lnum = 5;                           /**< Inflows number. */
 Real Lstart = (Wnum - 2 * Lnum + 2) / 4; /**< Start location of inlets. */
 Real Lend = (Wnum + 2 * Lnum - 2) / 4;   /**< End location of inlets. */
 Real inlet_height = RM - BW;             /**< Inflow location height */
 Real inlet_distance = -(0.5 * LL);          /**< Inflow location distance */
-Vec2d inlet_halfsize = Vec2d(0.5 * LL, 0.5 * LH);
-Vec2d inlet_translation = Vec2d(inlet_distance, inlet_height) + inlet_halfsize;
+Vec2d inlet_halfsize = Vec2d(0.5 * LH, 0.5 * LL);
+Vec2d inlet_translation = Vec2d(0, RM);
+Vec2d outlet_halfsize = Vec2d(0.5 * DO, 0.5 * OH);
+Vec2d outlet_translation = Vec2d(0, -RM);
 Transform inlet_transform(Rotation2d(-(Pi / 2)), inlet_translation);
 BoundingBox system_domain_bounds(Vec2d(-BW - RM, -BW - RM), Vec2d(RM + BW, RM + BW));
 Vecd center(0.0, 0.0);
@@ -46,9 +51,16 @@ Real gravity_g = 1.0;                                   /**< Gravity force of fl
 Real U_f = 2.0 * sqrt(gravity_g * (inlet_height + LH)); /**< Characteristic velocity. */
 Real c_f = 10.0 * U_f;                                  /**< Reference sound speed. */
 //----------------------------------------------------------------------
-//	Geometries
+//	Geometrie of the othor 4 inlets.
 //----------------------------------------------------------------------
-
+Vec2d inlet2_translation = Vec2d(RM * cos(5 * angle_increment), RM *sin(5 * angle_increment));
+Transform inlet2_transform(Rotation2d(-(angle_increment)), inlet2_translation);
+Vec2d inlet3_translation = Vec2d(RM * cos(4 * angle_increment), RM *sin(4 * angle_increment));
+Transform inlet3_transform(Rotation2d(-(2 * angle_increment)), inlet3_translation);
+Vec2d inlet4_translation = Vec2d(RM * cos(2 * angle_increment), RM *sin(2 * angle_increment));
+Transform inlet4_transform(Rotation2d(-(4 * angle_increment)), inlet4_translation);
+Vec2d inlet5_translation = Vec2d(RM * cos(angle_increment), RM *sin(angle_increment));
+Transform inlet5_transform(Rotation2d(-(5 * angle_increment)), inlet5_translation);
 //----------------------------------------------------------------------
 //	Case-dependent wall boundary
 //----------------------------------------------------------------------
@@ -59,10 +71,14 @@ class WallBoundary : public MultiPolygonShape
     {
         multi_polygon_.addACircle(center, (RM + BW) , resolution_circle, ShapeBooleanOps::add);         /**< Outer wall of motor hausing. */
         multi_polygon_.addACircle(center, RM , resolution_circle, ShapeBooleanOps::sub);                /**< Inner wall of motor hausing. */
-        multi_polygon_.addABox(inlet_transform, inlet_halfsize, ShapeBooleanOps::sub);     /**< Inlets. */
+        multi_polygon_.addABox(inlet_transform , inlet_halfsize, ShapeBooleanOps::sub);                 /**< Top Inlets. */
+        multi_polygon_.addABox(Transform(outlet_translation), outlet_halfsize, ShapeBooleanOps::sub);   /**< Outlets. */
+        multi_polygon_.addABox(inlet2_transform, inlet_halfsize, ShapeBooleanOps::sub);
+        multi_polygon_.addABox(inlet3_transform, inlet_halfsize, ShapeBooleanOps::sub);
+        multi_polygon_.addABox(inlet4_transform, inlet_halfsize, ShapeBooleanOps::sub);
+        multi_polygon_.addABox(inlet5_transform, inlet_halfsize, ShapeBooleanOps::sub);
         multi_polygon_.addACircle(center, RS , resolution_circle, ShapeBooleanOps::add);                /**< Shaft. */
         /** Add the windings. */
-        Real angle_increment = 2 * PI / Wnum;
         for (int i = 0; i < Wnum; ++i)
         {
             Real theta = i * angle_increment;
@@ -73,6 +89,21 @@ class WallBoundary : public MultiPolygonShape
             Transform winding_transform(Rotation2d(theta - (PI / 2)), winding_translation);
             multi_polygon_.addABox(winding_transform, winding_halfsize, ShapeBooleanOps::add);
         }
+    }
+};
+//----------------------------------------------------------------------
+//	Case-dependent Inletswater boundary
+//----------------------------------------------------------------------
+class FluidBoundary : public MultiPolygonShape
+{
+  public:
+    explicit FluidBoundary(const std::string &shape_name) : MultiPolygonShape(shape_name)
+    {
+        multi_polygon_.addABox(inlet_transform, inlet_halfsize, ShapeBooleanOps::add);                /**< Top Inlets. */
+        multi_polygon_.addABox(inlet2_transform, inlet_halfsize, ShapeBooleanOps::add);
+        multi_polygon_.addABox(inlet3_transform, inlet_halfsize, ShapeBooleanOps::add);
+        multi_polygon_.addABox(inlet4_transform, inlet_halfsize, ShapeBooleanOps::add);
+        multi_polygon_.addABox(inlet5_transform, inlet_halfsize, ShapeBooleanOps::add);
     }
 };
 //----------------------------------------------------------------------
@@ -103,8 +134,7 @@ int main(int ac, char *av[])
     //----------------------------------------------------------------------
     //	Creating body, materials and particles.
     //----------------------------------------------------------------------
-    TransformShape<GeometricShapeBox> oil_inlet_shape(inlet_transform, inlet_halfsize);
-    FluidBody oil_body(sph_system, oil_inlet_shape, "OilBody");
+    FluidBody oil_body(sph_system, makeShared<FluidBoundary>("OilBody"));
     oil_body.sph_adaptation_->resetKernel<KernelTabulated<KernelWendlandC2>>(20);
     oil_body.defineMaterial<WeaklyCompressibleFluid>(rho0_f, c_f);
     ParticleBuffer<ReserveSizeFactor> inlet_buffer(3500.0);
@@ -140,7 +170,7 @@ int main(int ac, char *av[])
     Dynamics1Level<fluid_dynamics::Integration1stHalfWithWallRiemann> pressure_relaxation(oil_body_inner, oil_body_contact);
     Dynamics1Level<fluid_dynamics::Integration2ndHalfWithWallRiemann> density_relaxation(oil_body_inner, oil_body_contact);
     InteractionWithUpdate<fluid_dynamics::DensitySummationComplexFreeSurface> update_density_by_summation(oil_body_inner, oil_body_contact);
-
+    
     Gravity gravity(Vecd(0.0, -gravity_g));
     SimpleDynamics<GravityForce> constant_gravity(oil_body, gravity);
     ReduceDynamics<fluid_dynamics::AdvectionTimeStepSize> get_fluid_advection_time_step_size(oil_body, U_f);
@@ -149,6 +179,18 @@ int main(int ac, char *av[])
     BodyAlignedBoxByParticle emitter(oil_body, makeShared<AlignedBoxShape>(xAxis, inlet_transform, inlet_halfsize));
     SimpleDynamics<InletInflowCondition> inflow_condition(emitter);
     SimpleDynamics<fluid_dynamics::EmitterInflowInjection> emitter_injection(emitter, inlet_buffer);
+    BodyAlignedBoxByParticle emitter2(oil_body, makeShared<AlignedBoxShape>(xAxis, inlet2_transform, inlet_halfsize));
+    SimpleDynamics<InletInflowCondition> inflow_condition2(emitter2);
+    SimpleDynamics<fluid_dynamics::EmitterInflowInjection> emitter_injection2(emitter2, inlet_buffer);
+    BodyAlignedBoxByParticle emitter3(oil_body, makeShared<AlignedBoxShape>(xAxis, inlet3_transform, inlet_halfsize));
+    SimpleDynamics<InletInflowCondition> inflow_condition3(emitter3);
+    SimpleDynamics<fluid_dynamics::EmitterInflowInjection> emitter_injection3(emitter3, inlet_buffer);
+    BodyAlignedBoxByParticle emitter4(oil_body, makeShared<AlignedBoxShape>(xAxis, inlet4_transform, inlet_halfsize));
+    SimpleDynamics<InletInflowCondition> inflow_condition4(emitter4);
+    SimpleDynamics<fluid_dynamics::EmitterInflowInjection> emitter_injection4(emitter4, inlet_buffer);
+    BodyAlignedBoxByParticle emitter5(oil_body, makeShared<AlignedBoxShape>(xAxis, inlet5_transform, inlet_halfsize));
+    SimpleDynamics<InletInflowCondition> inflow_condition5(emitter5);
+    SimpleDynamics<fluid_dynamics::EmitterInflowInjection> emitter_injection5(emitter5, inlet_buffer);
     //----------------------------------------------------------------------
     //	File output and regression check.
     //----------------------------------------------------------------------
@@ -193,15 +235,22 @@ int main(int ac, char *av[])
         {
             Real Dt = get_fluid_advection_time_step_size.exec();
             update_density_by_summation.exec();
-
             /** Dynamics including pressure relaxation. */
             Real relaxation_time = 0.0;
             while (relaxation_time < Dt)
             {
                 pressure_relaxation.exec(dt);
                 inflow_condition.exec();
+                inflow_condition2.exec();
+                inflow_condition3.exec();
+                inflow_condition4.exec();
+                inflow_condition5.exec();
                 density_relaxation.exec(dt);
                 inflow_condition.exec();
+                inflow_condition2.exec();
+                inflow_condition3.exec();
+                inflow_condition4.exec();
+                inflow_condition5.exec();
                 dt = get_fluid_time_step_size.exec();
                 relaxation_time += dt;
                 integration_time += dt;
@@ -218,6 +267,10 @@ int main(int ac, char *av[])
 
             /** inflow emitter injection*/
             emitter_injection.exec();
+            emitter_injection2.exec();
+            emitter_injection3.exec();
+            emitter_injection4.exec();
+            emitter_injection5.exec();
             /** Update cell linked list and configuration. */
 
             oil_body.updateCellLinkedListWithParticleSort(100);
